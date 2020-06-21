d5f9394ce2d6a03f33bfd7a4eb4f9ab46ebd0825
wangjiaqiys <1130097862@qq.com>
2020-06-21 22:55:57 +0800

    update code(TODO)

--- work_of_word2vec/utils/data_reader.py
+++ work_of_word2vec/utils/data_reader.py
@@ -1,14 +1,27 @@
 from collections import defaultdict
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = '/root/share/HCLG/ZN_qiye/week1/HCLG'
 
-
+"""
 def save_word_dict(vocab, save_path):
     with open(save_path, 'w', encoding='utf-8') as f:
         for line in vocab:
             w, i = line
             f.write("%s\t%d\n" % (w, i))
-
+"""
+def save_word_dict(vocab, save_path, sflag):
+    # TODO: 增加sflag，sflag=0 - save vocab; sflag=1 - save reverse_vocab
+    if sflag == 0:
+        with open(save_path, 'w', encoding='utf-8') as f:
+            for line in vocab:
+                w, i = line
+                f.write("%s\t%d\n"%(w, i))
+    elif sflag == 1:
+        with open(save_path, 'w', encoding='utf-8') as f:
+            for line in vocab:
+                i, w = line
+                f.write("%d\t%s\n"%(i, w))
 
 def read_data(path_1, path_2, path_3):
     with open(path_1, 'r', encoding='utf-8') as f1, \
@@ -52,6 +65,8 @@ def build_vocab(items, sort=True, min_count=0, lower=False):
         按照字典里的词频进行排序，出现次数多的排在前面
         your code(one line)
         """
+        # TODO:
+        dic = sorted(dic.items(), key=lambda x: x[-1], reverse=True)
         for i, item in enumerate(dic):
             key = item[0]
             if min_count and min_count > item[1]:
@@ -60,7 +75,7 @@ def build_vocab(items, sort=True, min_count=0, lower=False):
     else:
         # sort by items
         for i, item in enumerate(items):
-            item = item if not lower else item.lower()
+            item = item if not lower else item.lower() # 'T'.lower(), '我'.lower()->'我'
             result.append(item)
     """
     建立项目的vocab和reverse_vocab，vocab的结构是（词，index）
@@ -68,13 +83,16 @@ def build_vocab(items, sort=True, min_count=0, lower=False):
     vocab = (one line)
     reverse_vocab = (one line)
     """
+    # TODO:
+    vocab = [(word, value) for value, word in enumerate(result)]
+    reverse_vocab = [(value, word) for value, word in enumerate(result)]
 
     return vocab, reverse_vocab
 
 
 if __name__ == '__main__':
-    lines = read_data('{}/datasets/train_set.seg_x.txt'.format(BASE_DIR),
-                      '{}/datasets/train_set.seg_y.txt'.format(BASE_DIR),
-                      '{}/datasets/test_set.seg_x.txt'.format(BASE_DIR))
+    lines = read_data('{}/data/train_set.seg_x.txt'.format(BASE_DIR),
+                      '{}/data/train_set.seg_y.txt'.format(BASE_DIR),
+                      '{}/data/test_set.seg_x.txt'.format(BASE_DIR))
     vocab, reverse_vocab = build_vocab(lines)
-    save_word_dict(vocab, '{}/datasets/vocab.txt'.format(BASE_DIR))
+    save_word_dict(reverse_vocab, '{}/data/reverse_vocab.txt'.format(BASE_DIR), 1)
